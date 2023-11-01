21eb5973e3a08abab9c78713c5ebde69c7c314ff
sowai00 <72919700+sowai00@users.noreply.github.com>
2023-11-01 23:35:42 +0800

    add post

---
+++ pages/api/post.ts
@@ -0,0 +1,25 @@
+import { prisma } from "../../lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { title, content } = req.body;
+
+  try {
+    console.log(`${title} and ${content}`);
+    await prisma.note.create({
+      data: {
+        title: title,
+        content: content,
+      },
+    });
+    res.status(200).json({ message: "Note Created" });
+    console.log("no server error");
+  } catch (err) {
+    console.log("error server");
+    res.status(408).json({ message: "Request Timeout" });
+    console.error(err);
+  }
+}
