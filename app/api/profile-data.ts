8e11e94cb77d27a7565da6acd1a48658e508cece
sowai00 <72919700+sowai00@users.noreply.github.com>
2023-11-14 20:13:59 +0800

    api

--- app/api/profile-data.ts
+++ app/api/profile-data.ts
@@ -1,23 +1,13 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import { prisma } from "../../lib/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { title, content } = req.body;
-
+  const {} = req.body;
+  //need to check back the author then return and save the post
   try {
-    alert("processing");
-    console.log(`${title} and ${content}`);
-
-    await prisma.note.create({
-      data: {
-        title: title,
-        content: content,
-      },
-    });
-    alert("done");
     res.status(200).json({ message: "Note Created" });
     console.log("no server error");
   } catch (err) {
