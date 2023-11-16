e4f81459a5f95e5432378877eba4786d032f1fc2
sowai00 <72919700+sowai00@users.noreply.github.com>
2023-11-16 21:53:02 +0800

    prisma

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./navbar";
 import Footer from "./footer";
 //nav bar have problem
+//prisma
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
